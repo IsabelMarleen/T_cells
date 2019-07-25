a890e5af92ba47c8f94771ac14ca6b0786345f48
IsabelMarleen <isabel-marleen@t-online.de>
2019-07-25 17:23:43 +0200

    Added filtering and UMAP plots

--- Analysis.R
+++ Analysis.R
@@ -12,6 +12,9 @@ library( furrr )
 library( tidyverse )
 library(scales)
 source( "spmvar.R" )
+source("~/Desktop/sc_methods_dev/src/functions_universal.R")
+source("~/Desktop/sc_methods_dev/src/functions_specific.R")
+
 
 #Load data
 
@@ -25,9 +28,11 @@ FL4 <- Seurat::Read10X( "~/Desktop/T Cells/FL4")
 rLN1<- Seurat::Read10X( "~/Desktop/T Cells/rLN1")
 rLN2 <- Seurat::Read10X( "~/Desktop/T Cells/rLN2")
 rLN3 <- Seurat::Read10X( "~/Desktop/T Cells/rLN3")
+tFL1 <- Seurat::Read10X( "~/Desktop/T Cells/tFL1")
+tFL2 <- Seurat::Read10X( "~/Desktop/T Cells/tFL2")
 
 # Make list of sample names
-samplenames <- c(DLBCL1, DLBCL2, DLBCL3, FL1, FL2, FL3, FL4, rLN1, rLN2,  rLN3)
+samplenames <- c(DLBCL1, DLBCL2, DLBCL3, FL1, FL2, FL3, FL4, rLN1, rLN2,  rLN3, tFL1, tFL2)
 names(samplenames) <- names(data)
 
 # Function to calculate PCA, UMAP, colSums for a sample
@@ -68,12 +73,14 @@ data$FL4 <- calc_umap_etc(FL4)
 data$rLN1 <- calc_umap_etc(rLN1)
 data$rLN2 <- calc_umap_etc(rLN2)
 data$rLN3 <- calc_umap_etc(rLN3)
+data$tFL1 <- calc_umap_etc(tFL1)
+data$tFL2 <- calc_umap_etc(tFL2)
 
 
 #Function that adds raw and smoothed gene expression to data
 
-sample <- DLBCL1
-samplename <- "DLBCL1"
+sample <- tFL1
+samplename <- "tFL1"
 gene <- "CD3E"
 add_gene <- function( gene ) {
     for( samplename in names(data) ) {
@@ -100,6 +107,7 @@ add_gene("MT1A")
 add_gene("MT2A")
 add_gene("CD4")
 add_gene("CD8B")
+add_gene("CD79B")
 
 #Sorting into categories
 #Creating a list of interesting genes
@@ -111,7 +119,7 @@ genelist <- c("CD3E", "GZMK", "CD4", "CD8B", "PDCD1", "LAG3", "HAVCR2", "GATA3",
 #Creating new list
 location <- list()
 for (gene in genelist) {
-  for( s in names(data) ) {
+  for( s in names(data)) {
     a <- data[[s]][[paste0("smooth_", gene)]]
     a <- a[ a < .9 ]
     location[[s]][[paste0("locmodes", gene)]] <- multimode::locmodes( a ^.15, 2 )$location^(1/.15)
@@ -119,31 +127,46 @@ for (gene in genelist) {
   }
 }
 
-#Trying different powertransformation
+#Trying different powertransformation #Trying more modes
 location.2 <- list()
 for (gene in genelist) {
   for( s in names(data) ) {
     a <- data[[s]][[paste0("smooth_", gene)]]
     a <- a[ a < .9 ]
-    location.2[[s]][[paste0("locmodes", gene)]] <- multimode::locmodes( a ^.05, 2 )$location^(1/.05)
-    location.2[[s]][[paste0("thresh", gene)]] <- location.2[[s]][[paste0("locmodes", gene)]][2]
+    location.2[[s]][[paste0("locmodes", gene)]] <- multimode::locmodes( a ^.2, 3 )$location^(1/.2)
+    location.2[[s]][[paste0("thresh", gene)]] <- location.2[[s]][[paste0("locmodes", gene)]][4]
   }
 }
 
+#Doing multimode only on CD3E pos cells
+s <- samplename
+location.3 <- list()
+for (gene in genelist) {
+  for( s in names(data)[11:12] ) {
+    a <- data[[s]][[paste0("smooth_", gene)]]
+    a <- a[CD3Epos[[s]]]
+    a <- a[ a < .9 ]
+    location.3[[s]][[paste0("locmodes", gene)]] <- multimode::locmodes( a ^.15, 2 )$location^(1/.15)
+    location.3[[s]][[paste0("thresh", gene)]] <- location.3[[s]][[paste0("locmodes", gene)]][2]
+  }
+}
 
 #Produce histograms
-for (sample in names(data)){
-  for (gene in genelist[3]) {
-    hist(data[[sample]][[paste0("smooth_", gene)]][data[[sample]][[paste0("smooth_", gene) ]] < .9]^.15, main=c(gene, sample), breaks=100)
+for (sample in names(data)[2]){
+  for (gene in genelist[2]) {
+    hist(data[[sample]][[paste0("smooth_", gene)]][data[[sample]][[paste0("smooth_", gene) ]] < .9]^.15, main=c(gene, sample, "mit B Zellen"), breaks=100)
     abline(v=location[[sample]][[paste0("locmodes", gene)]]^.15, col="yellow")
   }
 }
 
 #only FL4
-for (sample in names(data)[10]){
-  for (gene in genelist[3]) {
-    hist(data[[sample]][[paste0("smooth_", gene)]][data[[sample]][[paste0("smooth_", gene) ]] < .9]^.15, main=c(gene, sample), breaks=100)
-    abline(v=location[[sample]][[paste0("locmodes", gene)]]^.15, col="yellow")
+for (sample in names(data)[2]){
+  for (gene in genelist[2]) {
+    da <- data[[sample]][[paste0("smooth_", gene)]][ CD3Epos[[ sample ]] ]
+    da <- da[da < .9] 
+    da <- da^.15 %>%
+    hist(main=c(gene, sample), breaks=100)
+    abline(v=location.3[[sample]][[paste0("locmodes", gene)]]^.15, col="yellow")
   }
 }
 
@@ -169,6 +192,7 @@ location$FL4$threshCD8Bman <- 0.25^(1/.15)
 #   heatmap(., scale="none")
 
 numerise_smooth <- function( gene ) {
+  #Will give error when using for samples that are not FL4, no manual CD8 cutoff
   sampleDF <- data[[ sample ]][ data[[ sample ]][[ "smooth_CD3E" ]] > location[[ sample ]][[ "threshCD3E" ]], ]
   if (gene == "CD4" || gene == "CD8B") {
     return( as.numeric(sampleDF[, paste0("smooth_", gene)] > location[[sample]][[paste0("thresh", gene, "man")]]) )
@@ -179,8 +203,48 @@ numerise_smooth <- function( gene ) {
 }
 
 
-for (sample in names(data)[7]) {
+for (sample in names(data)) {
   heatmap( sapply(genelist, numerise_smooth), scale ="none", main=sample)
 }
 
+
+#Filter for CD3E and GZMK pos and B cell marker CD79B
+filter_gene_pos <- function(sample, gene) {
+  Genepos<- data[[sample]][[paste0("smooth_", gene)]] > location[[sample]][[paste0("thresh", gene)]]
+}
+
+
+CD3Epos <- sapply(names(data), filter_gene_pos, "CD3E")
+GZMKpos <- sapply(names(data), filter_gene_pos, "GZMK")
+
+filter_gene_neg <- function(sample, gene) {
+  Geneneg<- data[[sample]][[paste0("smooth_", gene)]] < location[[sample]][[paste0("thresh", gene)]]
+}
+
+CD79Bneg <- sapply(names(data), filter_gene_neg, "CD79B")
+
+#Look at T cell division (CD3E pos/neg) in UMAP
+lapply(names(data), function(i){
+  x <- data[[i]]
+  data.frame(
+    sample = i,
+    UMAP1 = x$UMAP1,
+    UMAP2 = x$UMAP2,
+    CD3Epos = CD3Epos[[i]],
+    GZMKpos = GZMKpos[[i]],
+    stringsAsFactors = FALSE)
+}) %>% 
+  bind_rows() %>%
+  mutate(class=case_when(
+                        CD3Epos & GZMKpos ~ "T_tox",
+                        CD3Epos & !GZMKpos ~ "T_CD4",
+                        TRUE ~ "other")) %>%
+ggplot(aes( UMAP1, UMAP2,
+            col= class))+
+  geom_point( size=0.2)+
+  facet_wrap(~sample)
+
+
+
+#
 
